105efc097f0b5f02fddce6c639cc6537b22dc78c
Gabriel Kauer <gjmkauer@hotmail.com>
2022-08-25 16:17:00 -0300

    Adiciona model User.

---
+++ src/database/models/user.js
@@ -0,0 +1,25 @@
+/**
+* 
+* @param {import('sequelize').Sequelize} sequelize
+* @param {import('sequelize').DataTypes} DataTypes
+*/
+
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+    },
+    displayName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    image: DataTypes.STRING,
+  }, {
+    updatedAt: false,
+    tableName: 'Users',
+  });
+
+  return User;
+};
